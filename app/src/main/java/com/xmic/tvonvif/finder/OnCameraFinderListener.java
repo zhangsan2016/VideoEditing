538f124b1fe15bfc5ebf92df106ee12b4f1e7730
liu <602694077@qq.com>
2019-06-01 10:35:56 +0800

    2019年6月1日 10:33:33  存在内存溢出和读不到os文件问题（os文件无法读取原因是sdk版本问题，os没有对应的64位os文件所以无法适用高版本）

---
+++ app/src/main/java/com/xmic/tvonvif/finder/OnCameraFinderListener.java
@@ -0,0 +1,5 @@
+package com.xmic.tvonvif.finder;
+
+public interface OnCameraFinderListener {
+    public void OnCameraListUpdated();
+}
---
+++ app/src/main/java/com/xmic/tvonvif/finder/OnSoapDoneListener.java
@@ -0,0 +1,5 @@
+package com.xmic.tvonvif.finder;
+
+public interface OnSoapDoneListener {
+	void onSoapDone(CameraDevice device, boolean success);
+}
